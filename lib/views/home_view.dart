1d92808aa9ddfe3e078ddae093f4d0f6b14f55ba
KM-Fathy <kareem.eissa88@gmail.com>
2026-09-02 00:41:06 +0300

    add categories list view

---
+++ lib/views/home_view.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/widgets/categories_list_view.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'News',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Cloud',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.orange,
+              ),
+            ),
+          ],
+        ),
+        centerTitle: true,
+      ),
+      body: const CategoriesListView(),
+    );
+  }
+}
