43733c2f3d6d36116d93d94b0243f2bff9c4c7f1
KM-Fathy <kareem.eissa88@gmail.com>
2026-09-09 00:33:54 +0300

    refactor UI code

--- lib/widgets/news_list_view.dart
+++ lib/widgets/news_list_view.dart
@@ -1,29 +1,11 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/models/article_model.dart';
-import 'package:news_app/services/news_service.dart';
 import 'package:news_app/widgets/news_tile.dart';
 
-class NewsListView extends StatefulWidget {
-  const NewsListView({super.key});
+class NewsListView extends StatelessWidget {
+  final List<ArticleModel> articles;
 
-  @override
-  State<NewsListView> createState() => _NewsListViewState();
-}
-
-class _NewsListViewState extends State<NewsListView> {
-  List<ArticleModel> articles = [];
-
-  @override
-  void initState() {
-    super.initState();
-    getGeneralNews();
-  }
-
-  Future<void> getGeneralNews() async {
-    articles = await NewsService(Dio()).getNews();
-    setState(() {});
-  }
+  const NewsListView({super.key, required this.articles});
 
   @override
   Widget build(BuildContext context) {
